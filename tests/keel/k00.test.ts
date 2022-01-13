b4110c9bfd7125b75f933e744c3007a41ec46d7b
lunz1207 <lunz1207@gmail.com>
2022-01-13 11:25:32 +0800

    chore: exmple case

--- tests/keel/k00.test.ts
+++ tests/keel/k00.test.ts
@@ -9,7 +9,17 @@ it("add rope", (done) => {
     .post(`/apis/rudder/v1/repos/${keel.repoName[0]}`)
     .set("authorization", ironMan.authorization)
     .send("https://tkeel-io.github.io/helm-charts")
-    .expect(200, done);
+    .expect(200) // 最基本的断言，仅仅判断 status_code
+    .then((res) => {
+      // 在回调中做更多的断言
+      let result = JSON.parse(res.text).data;
+      expect(result).toContain({ code: 200 });
+
+      // 在回调中绑定数据到全局变量 ironMan, 其他用例中使用 ironMan.test 调用
+      // 注意：需要保证用例的执行顺序
+      ironMan["test"] = "any word";
+      done();
+    });
 });
 
 /**
